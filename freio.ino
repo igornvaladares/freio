9e13d177291c51c44cd72c44988eb879c122fbc4
igornvaladares <62352694+igornvaladares@users.noreply.github.com>
2025-07-19 16:17:25 -0300

    Update freio.ino

--- freio.ino
+++ freio.ino
@@ -1,19 +1,22 @@
 #include <EEPROM.h>
+#include <SoftwareSerial.h>
+
 // === PINOS ===
+const int botaoPin = 2;             // Botão (INT0)
+const int sensorPeFreioPin = 4;    
+const int sensorPortaPin = 8;       
+const int ledStatus = 10;
+SoftwareSerial receptorStatusCarro(A4,255); // RX A4; TX 255
+
+const int PWM = 11;  // Ligar os pinos R_ENA e L_ENA no D11 do arduino
 const int L_PWM = 12;
 const int R_PWM = 13;
-const int PWM = 11;  // Ligar os pinos R_ENA e L_ENA no D11 do arduino
 const int sinalPainelStatus = 9;
 
-const int ledStatus = 10;
-const int botaoPin = 2;             // Botão (INT0)
-const int sensorPortaPin = 8;       
-const int sensorPeFreioPin = 4;    
-const int sensorCarroParadoPin = 3; // Carro parado = sinal pulsante
 
 const int correntePin1 = A0; // Sensor da pont H
 const int correntePin2 = A1; // Sensor da pont H
-const int entradaStatusCarro = A4; // Dados vindo do arquino de cambio
+//const int entradaStatusCarro = A4; // Dados vindo do arquino de cambio
 
 // === TEMPOS ===
 const int TEMPO_50 = 1000;   // ms
@@ -29,26 +32,32 @@ const int EEPROM_ADDR = 0;
 bool SENTIDO_FREIANDO = true;
 
 bool freiado = false;
-int PARADO_EM_NEUTRO = 1;
-int PARADO_ENGRENADO = 2;
-int EM_MOVIMENTO = 3; // a partir de 10km/h
+uint8_t PARADO_EM_NEUTRO = 1;
+uint8_t PARADO_ENGRENADO = 2;
+uint8_t EM_MOVIMENTO = 3; // a partir de 10km/h
 
 bool bloqueadoAcionamentoAutomatico = false;
 
 void setup() {
+  pinMode(sensorPortaPin, OUTPUT);
+  digitalWrite(sensorPortaPin, HIGH);
+  delay(250);
+  pinMode(sensorPortaPin, INPUT_PULLUP);
+  
+  pinMode(sensorPeFreioPin, OUTPUT);
+  digitalWrite(sensorPeFreioPin, HIGH);
+  delay(250);
+  pinMode(sensorPeFreioPin, INPUT_PULLUP);
+  
   pinMode(L_PWM, OUTPUT);
   pinMode(R_PWM, OUTPUT);
   pinMode(PWM, OUTPUT);
-  
   pinMode(ledStatus, OUTPUT);
   pinMode(sinalPainelStatus, OUTPUT);
-
   pinMode(botaoPin, INPUT_PULLUP);
-  pinMode(sensorPortaPin, INPUT_PULLUP);
-  pinMode(sensorPeFreioPin, INPUT_PULLUP);
-  pinMode(sensorCarroParadoPin, INPUT_PULLUP);
-
-  Serial.begin(9600);
+ 
+  //Serial.begin(9600);
+  receptorStatusCarro.begin(9600);
   Serial.println("INICIADO.");
   
   freiado = EEPROM.read(EEPROM_ADDR) > 0;
@@ -78,7 +87,8 @@ void piscarLed(){
         digitalWrite(ledStatus, !statusLed);
      }
   
-  }
+}
+  
 void aguardar(float tempoAgurde) {
     long tempoInicio = millis();
     Serial.print("aguarde  por:");
@@ -91,7 +101,7 @@ void aguardar(float tempoAgurde) {
 }
 
 void loop() {
-  int statusCarro = detectarStatusCarro();
+  uint8_t statusCarro = detectarStatusCarro();
   verificarAcionamentoManual(statusCarro);
   verificarAcionamentoAutomatico(statusCarro);
 
@@ -135,9 +145,12 @@ void finalizarFreiar(int intensidade) {
 }
 
 // === LEITURA DO BOTÃO COM CLIQUE CURTO/LONGO ===
-void verificarAcionamentoManual(int statusCarro) {
+void verificarAcionamentoManual(uint8_t statusCarro) {
+  Serial.println("Loop");
+  
   bool peFreio = digitalRead(sensorPeFreioPin) == LOW;
   if (peFreio && botaoApertado()){
+    bloqueadoAcionamentoAutomatico =true;
     long tempoInicio = millis();
     // Se ultrapassar o tempoAguarde ou botão foi solto
     while (botaoApertado()){
@@ -147,9 +160,9 @@ void verificarAcionamentoManual(int statusCarro) {
        }  
     }
     if (freiado){
-        desativarFreio(true);
+        desativarFreio();
     }else{
-      if (statusCarro!= EM_MOVIMENTO){ // Bloqueia o click simples com carro em movimento. 
+      if (statusCarro != EM_MOVIMENTO){ // Bloqueia o click simples com carro em movimento. 
         iniciarFreiar(); 
         if (botaoNaoApertado())
             finalizarFreiar(50);
@@ -159,14 +172,12 @@ void verificarAcionamentoManual(int statusCarro) {
   }
 }
 // === AÇÃO AUTOMÁTICA DE SEGURANÇA ===
-void verificarAcionamentoAutomatico(int statusCarro) { 
+void verificarAcionamentoAutomatico(uint8_t statusCarro) { 
   bool peFreio = digitalRead(sensorPeFreioPin) == LOW;
   bool portaAberta = digitalRead(sensorPortaPin) == LOW;
   bool portaFechada = digitalRead(sensorPortaPin) == HIGH;
   
-  Serial.print("Status CArro:");
-  Serial.println(statusCarro);
-  
+ 
   if (portaAberta && !peFreio && statusCarro== PARADO_ENGRENADO && !freiado && !bloqueadoAcionamentoAutomatico) { // CARRO PARADO EM DRIVE
      Serial.println("Ação automática de Segurança: Porta Aberta, Carro parado em Drive");
      freiar(50);
@@ -179,7 +190,7 @@ void verificarAcionamentoAutomatico(int statusCarro) {
   }
   if (statusCarro == PARADO_ENGRENADO && portaFechada && freiado && peFreio && !bloqueadoAcionamentoAutomatico) { // CARRO PARADO EM DRIVE
      Serial.println("Ação automática de Segurança: Carro parado em Drive");
-     desativarFreio(false);
+     desativarFreio();
   }
 
 
@@ -190,30 +201,21 @@ void verificarAcionamentoAutomatico(int statusCarro) {
 }
 
 // === DETECÇÃO DE CARRO PARADO N e CARRO PARADO ENGATADO) ===
-signed char detectarStatusCarro() {
-    int leituraStatusCarro = leituraMediaAnologica(entradaStatusCarro);
-    int voltStatusCarro = map(leituraStatusCarro,0,1023,0,500);
-    //Serial.print("Volts:");
-    //Serial.println(leituraStatusCarro);
-    signed char statusCarro =0;
-    switch (voltStatusCarro) {
-        case 100  ... 200: // valor exato é 150
-          statusCarro = PARADO_EM_NEUTRO; // Carro Parado em N
-        break;
-        case 201 ... 300: // Valor é 250
-          statusCarro = PARADO_ENGRENADO;// Carro Parado engatado
-          break;
-        case 301 ... 400:// Valor é 350
-          statusCarro =EM_MOVIMENTO;// Carro em movimento >= 10km/h
-        break;
+uint8_t detectarStatusCarro() {
+  static uint8_t statusCarro =0;
+  if (receptorStatusCarro.available()){
+      statusCarro = receptorStatusCarro.read();
+      Serial.print("Status CArro:");
+      Serial.println(statusCarro);
+  
     }
-    return statusCarro;
+  return statusCarro;
 }
 
 // === DESATIVA FREIO ===
-void desativarFreio(bool desativarAcionamentoAutomatico) {
+void desativarFreio() {
   if (!freiado) return;
-  bloqueadoAcionamentoAutomatico = desativarAcionamentoAutomatico;
+  
   iniciar(!SENTIDO_FREIANDO);
   Serial.println("Desativando freio...");
   aguardar(EEPROM.read(EEPROM_ADDR) == 100 ? TEMPO_100: TEMPO_50);
@@ -221,15 +223,17 @@ void desativarFreio(bool desativarAcionamentoAutomatico) {
   digitalWrite(ledStatus, LOW);
   digitalWrite(sinalPainelStatus, HIGH);
   EEPROM.write(EEPROM_ADDR,0);
+  
   parar();
 }
 
 float leituraMediaAnologica(int pinAnalogico){
   long soma=0;
-   for(int i=1;i<=20;i++){
+   for(int i=1;i<=10;i++){
        soma = soma + analogRead(pinAnalogico);
+     //  delay(5);
     }
-  return soma/20;
+  return soma/10;
 }
 
 void iniciar(bool sentido){
